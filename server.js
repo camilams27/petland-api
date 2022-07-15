9e38ca573337d4cc5c0f04b5746e0501dbcc6a62
Camila Silva <silva.camilam27@gmail.com>
2022-07-14 21:48:16 -0300

    deploy api petland

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const connectDatabase = require('./dataBase');
+const cors = require('cors');
+
+connectDatabase();
+
+const port = process.env.PORT || 3333;
+
+app.set("host", process.env.HOST || "localhost");
+// app.set('port', process.env.PORT || 3333);
+
+app.use(cors());
+
+app.use(express.json())
+app.use(routes);
+
+app.listen(process.env.PORT, ()=>{ console.log(`acesse: http://localhost:${port}`)});
